332ab4e4c02da89826a07a745ffce06c142fba58
Alexis <alexis.martinho.78@gmail.com>
2025-01-23 16:31:27 +0100

    Exercice : model matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "opengl-framework/opengl-framework.hpp" // Inclue la librairie qui va nous servir à faire du rendu
 #include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
 
 int main()
 {
@@ -59,7 +60,13 @@ int main()
 
         glm::mat4 const view_matrix = camera.view_matrix();
         glm::mat4 const projection_matrix = glm::infinitePerspective(glm::radians(90.f) /*field of view in radians*/, gl::framebuffer_aspect_ratio() /*aspect ratio*/, 0.001f /*near plane*/);
-        glm::mat4 const view_projection_matrix = projection_matrix * view_matrix;
+        glm::mat4 const rotation = glm::rotate(glm::mat4{1.f}, gl::time_in_seconds() /*angle de la rotation*/, glm::vec3{0.f, 0.f, 1.f} /* axe autour duquel on tourne */);
+        glm::mat4 const translation = glm::translate(glm::mat4{1.f}, glm::vec3{0.f, 1.f, 0.f} /* déplacement */);
+        // translation * rotation --> rotate le plane sur son centre
+        // rotation * translation --> rotate le plane autour d'un axe (z ici)
+        glm::mat4 const model_matrix = translation * rotation;
+
+        glm::mat4 const view_projection_matrix = projection_matrix * view_matrix * model_matrix;
 
         shader.set_uniform("rotation_speed", 1.f);
         shader.set_uniform("time", gl::time_in_seconds());
