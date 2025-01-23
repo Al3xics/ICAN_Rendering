2390a58cf10d427cde84c4b24f7511438b4ddc96
Alexis <alexis.martinho.78@gmail.com>
2025-01-23 15:09:18 +0100

    Draw Rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,29 @@ int main()
     gl::maximize_window(); // On peut la maximiser si on veut
 
     auto const triangle_mesh = gl::Mesh{{
-    .vertex_buffers = {{
-        .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
-        .data   = {
-            -1.f, -1.f, // Position2D du 1er sommet
-            +1.f, -1.f, // Position2D du 2ème sommet
-             0.f, +1.f  // Position2D du 3ème sommet
-        },
-    }},
-}};
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .data   = {
+                -1.f, -1.f, // Position2D du 1er sommet
+                +1.f, -1.f, // Position2D du 2ème sommet
+                0.f, +1.f  // Position2D du 3ème sommet
+            },
+        }},
+    }};
+
+    auto const rectangle_mesh = gl::Mesh{{
+        .vertex_buffers = {{
+            .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+            .data   = {
+                -0.5f, -0.5f,
+                0.5f, -0.5f,
+                -0.5f, 0.5f,
+                0.5f, -0.5f,
+                -0.5f, 0.5f,
+                0.5f, 0.5f
+            },
+        }},
+    }};
 
     while (gl::window_is_open())
     {
@@ -24,6 +38,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
-        triangle_mesh.draw();
+        // triangle_mesh.draw();
+        rectangle_mesh.draw();
     }
 }
