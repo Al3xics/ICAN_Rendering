bcc660a281448c2c5d206dd9011bcc67fbfdbbff
Alexis <alexis.martinho.78@gmail.com>
2025-01-23 14:36:48 +0100

    Blue Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,24 @@ int main()
     gl::init("TPs de Rendering"); // On crée une fenêtre et on choisit son nom
     gl::maximize_window(); // On peut la maximiser si on veut
 
+    auto const triangle_mesh = gl::Mesh{{
+    .vertex_buffers = {{
+        .layout = {gl::VertexAttribute::Position2D{0 /*Index de l'attribut dans le shader, on en reparle juste après*/}},
+        .data   = {
+            -1.f, -1.f, // Position2D du 1er sommet
+            +1.f, -1.f, // Position2D du 2ème sommet
+             0.f, +1.f  // Position2D du 3ème sommet
+        },
+    }},
+}};
+
     while (gl::window_is_open())
     {
         // Rendu à chaque frame
+        glClearColor(0.f, 0.f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        gl::bind_default_shader(); // On a besoin qu'un shader soit bind (i.e. "actif") avant de draw(). On en reparle dans la section d'après.
+        triangle_mesh.draw();
     }
 }
