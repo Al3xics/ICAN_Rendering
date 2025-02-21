e926a9d690a2709da67277229b640983770cb5f3
Alexis <alexis.martinho.78@gmail.com>
2025-02-21 15:31:33 +0100

    Cube texturé

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,8 +1,8 @@
 #version 410
 
-layout(location = 0) in vec2 in_position;
+// layout(location = 0) in vec2 in_position;
+layout(location = 0) in vec3 in_position;
 layout(location = 1) in vec2 in_uv;
-// layout(location = 0) in vec3 in_position;
 uniform float aspect_ratio = 2;
 uniform float time;
 uniform float rotation_speed;
@@ -12,19 +12,19 @@ out vec2 uv;
 
 void main()
 {
-    vec2 position = in_position;
+    // vec2 position = in_position;
     // position.x += 0.25;
     // position.y += 0.25;
     // position.x += sin(time * rotation_speed);
     // position.y += cos(time * rotation_speed);
-    position.x /= aspect_ratio;
+    // position.x /= aspect_ratio;
 
     uv = in_uv;
 
-    // vertex_position = in_position;
+    vertex_position = in_position;
 
     // gl_Position = view_projection_matrix * vec4(in_position, 0., 1.);
-    // gl_Position = view_projection_matrix * vec4(in_position, 1.);
+    gl_Position = view_projection_matrix * vec4(in_position, 1.);
 
-    gl_Position = vec4(position, 0., 1.);
+    // gl_Position = vec4(position, 0., 1.);
 }
